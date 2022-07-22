d99c3da5b8d61f9c2cba8dcaed2a7d1a39328e25
Ruchinna <102924739+Ruchcoder@users.noreply.github.com>
2022-07-22 01:33:17 +0100

    Add files via upload

---
+++ Day1/file.js
@@ -0,0 +1,56 @@
+Write a single line comment which says, //comments can make code readable
+
+Write another single comment which says, //Welcome to 30DaysOfJavaScript
+
+Write a multiline comment which says, /*comments can make code readable, easy to reuse and informative
+*/
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let name = 'ruth' //string
+let age = 23 //number
+let married  = true //boolean
+let pregnant = null //object
+let children  //undefined
+
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log (typeof 'ruth')
+console.log (typeof 23)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+Declare four variables without assigning values
+let name 
+let age 
+let married 
+let pregnant
+let children  
+
+Declare four variables with assigned values
+
+let name = "ruth" //string
+let age = 23 //number
+let married  = true //boolean
+let pregnant = null //object
+
+
+
+let myAge = 23
+let yourAge = 25
+
+console.log (`I am ${myAge} year old \n you are ${yourAge} year old`)
+
+/*Declare variables to store your first name, last name, marital status, country and age in a single line
+*/
+let firstName = "Ruth"
+let lastName = "Okeniyi"
+let maritalStatus = "married"
+let country  = "Germany"
+let age = 23
+let space =" "
+let fullName = firstName + space + lastName + space + 'is' + space + age + space + 'and' + space + maritalStatus + space + 'in' + space + country + space
+
+console.log (fullName)
---
+++ Day1/variable.js
@@ -0,0 +1,9 @@
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let name = 'ruth' //string
+let age = 23 //number
+let married  = true //boolean
+let pregnant = null //object
+let children  //undefined
+
